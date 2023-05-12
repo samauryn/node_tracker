22d8e18b9a50c2f6c3b6767d6e5d38522a017e11
samauryn <118288578+samauryn@users.noreply.github.com>
2023-05-13 00:53:02 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from 'express'
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/database';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express()
+const port = process.env.PORT || 3000
+
+const firebaseConfig = {
+    apiKey: process.env.YOUR_API_KEY,
+    authDomain: process.env.YOUR_AUTH_DOMAIN,
+    databaseURL: process.env.YOUR_DATABASE_URL,
+    projectId: process.env.YOUR_PROJECT_ID,
+    storageBucket: process.env.YOUR_STORAGE_BUCKET,
+    messagingSenderId: process.env.YOUR_MESSAGING_SENDER_ID,
+    appId: process.env.YOUR_APP_ID,
+    measurementId: process.env.YOUR_MEASUREMENT_ID
+};
+
+
+firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database();
+
+
+const dataRef = database.ref('UsersData/8HlmCNNF8zfNVWktHrVHFFKeRwu2/');
+let data = {}
+dataRef.on('value', (snapshot) => {
+    data = snapshot.val();
+}, (error) => {
+    console.error(error);
+});
+
+app.get('/', (req, res) => {
+    res.json(data)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
